990b0480052943dcfecc2f6d52ad5d98ee860a0d
developerdong <developerdong@gmail.com>
2019-11-27 14:01:01 +0800

    Use TCP query first

--- src/main/java/com/toby/aDNS/DNSResolver.java
+++ src/main/java/com/toby/aDNS/DNSResolver.java
@@ -4,15 +4,16 @@ import org.xbill.DNS.ExtendedResolver;
 import org.xbill.DNS.Message;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.UnknownHostException;
 import java.util.concurrent.Callable;
 
 public class DNSResolver implements Callable<Message> {
 
-    private Message qmsg, rmsg;
+    private Message qmsg;
     private ExtendedResolver resolver;
 
-    public DNSResolver(int timeout, Message msg, String server) throws UnknownHostException {
+    DNSResolver(int timeout, Message msg, String server) throws UnknownHostException {
         resolver = new ExtendedResolver(server.split(","));
         resolver.setTimeout(timeout);
         qmsg = msg;
@@ -20,13 +21,17 @@ public class DNSResolver implements Callable<Message> {
 
     @Override
     public Message call() throws Exception {
+        Message rmsg = null;
         try {
+            resolver.setTCP(true);
             rmsg = resolver.send(qmsg);
-            return rmsg;
+        } catch (ConnectException e) {
+            resolver.setTCP(false);
+            rmsg = resolver.send(qmsg);
         } catch (IOException e) {
             SimpleLog.log("Failed to resolve " + qmsg.getQuestion().getName() + ": " + e.toString());
-            return null;
         }
+        return rmsg;
     }
 
 }
